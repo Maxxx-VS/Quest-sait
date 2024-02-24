df0fec10d101406cbb50c37544227caac0e48842
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-02-24 17:59:43 +0600

    Реализован вывод новостей

--- questionnaire/news/views.py
+++ questionnaire/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
 
@@ -10,9 +10,15 @@ def news_home(request):
 
 def create(request):
     if request.method == 'POST':
-
+        form = ArticlesForm(request.POST)
+        if form.is_valid(): #проверяет корректность заполненых данных
+            form.save()
+            return redirect('news_home')
+        else:
+            er = 'Форма была неверной'
     form = ArticlesForm()
     data = {
-        'form': form
+        'form': form,
+
     }
     return render(request, "news/create.html", data)
