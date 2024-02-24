afa355435c59c204dc0ec4f5fdff17385074b77b
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-02-24 17:39:42 +0600

    Реализован вывод новостей

--- questionnaire/news/views.py
+++ questionnaire/news/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .models import Articles
+from .forms import ArticlesForm
 
-def news_home(reques):
-    return render(reques, 'main/about.html')
+def news_home(request):
+    # news = Articles.objects.all() #сортировка новостей
+    # news = Articles.objects.order_by('title')[:1]
+    news = Articles.objects.order_by('-date')
+    return render(request, 'news/news_home.html', {'news': news})
+
+def create(request):
+    if request.method == 'POST':
+
+    form = ArticlesForm()
+    data = {
+        'form': form
+    }
+    return render(request, "news/create.html", data)
