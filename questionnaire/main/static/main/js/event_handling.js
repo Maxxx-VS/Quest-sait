08b1c52faedecd03b3f44bbe6133bb1496ce9b06
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-02-25 14:31:49 +0600

    Добавил JS

---
+++ questionnaire/main/static/main/js/event_handling.js
@@ -0,0 +1,16 @@
+function count_1(){
+    let newBtn = document.createElement('button');
+    newBtn.innerText="Вариант-1";
+    document.querySelector('#button-container').appendChild(newBtn);
+    console.log(document.querySelector("selector"));
+
+    }
+
+
+function count_2(){
+    let newBtn = document.createElement('button');
+    newBtn.innerText="Вариант-2";
+    document.querySelector('#button-container').appendChild(newBtn);
+    console.log(document.querySelector("selector"));
+
+    }
